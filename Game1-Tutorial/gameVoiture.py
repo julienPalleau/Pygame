a840db53dcae70514ef65931d27a582338dd9093
julienPalleau <jpalleau@gmail.com>
2022-07-21 16:11:48 +0200

    re-arrange all the tutos

--- Game1-Tutorial/gameVoiture.py
+++ Game1-Tutorial/gameVoiture.py
@@ -1,38 +1,106 @@
-import pygame, sys
-from pygame.locals import *
 import random
 
+import pygame
+import sys
+from pygame.locals import (
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT,
+)
+
+# Initializing
 pygame.init()
 
+# Setting up FPS
 FPS = 60
 FramePerSec = pygame.time.Clock()
 
+# Creating colors
 BLUE = (0, 0, 255)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
-# Screen information
+# Other Variables for use in the program
 SCREEN_WIDTH = 400
 SCREEN_HEIGHT = 600
+SPEED = 5
 
-DISPLAYSURF = pygame.display.set_mode((400, 600))
+# Create a white screen
+DISPLAYSURF = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 DISPLAYSURF.fill(WHITE)
 pygame.display.set_caption("Game")
+
 
 class Enemy(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
         self.image = pygame.image.load("Enemy.png")
         self.rect = self.image.get_rect()
-        self.rect.center = (random.randint(40, SCREEN_WIDTH-40), 0)
+        self.rect.center = (random.randint(40, SCREEN_WIDTH - 40), 0)
 
     def move(self):
-        self.rect.move_ip(0,10)
-        if (self.rec.bottom > 600):
+        self.rect.move_ip(0, SPEED)
+        if self.rect.bottom > 600:
             self.rect.top = 0
             self.rect.center = (random.randint(30, 370), 0)
 
     def draw(self, surface):
         surface.blit(self.image, self.rect)
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("Player.png")
+        self.rect = self.image.get_rect()
+        self.rect.center = (160, 500)
+
+    def update(self):
+        pressed_keys = pygame.key.get_pressed()
+        # if pressed_keys[K_UP]:
+        # self.rect.move_ip(0, -5)
+        # if pressed_keys[K_DOWN]:
+        # self.rect.move_ip(0,5)
+
+        if self.rect.left > 0:
+            if pressed_keys[K_LEFT]:
+                self.rect.move_ip(-5, 0)
+        if self.rect.right < SCREEN_WIDTH:
+            if pressed_keys[K_RIGHT]:
+                self.rect.move_ip(5, 0)
+
+    def draw(self, surface):
+        surface.blit(self.image, self.rect)
+
+# Setting up Sprites
+P1 = Player()
+E1 = Enemy()
+
+# Game Loop
+while True:
+
+    # Cycles through all events occuring
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+
+        if event.type == KEYDOWN:
+            # If the Esc is pressed, then exit the main loop
+            if event.key == K_ESCAPE:
+                pygame.quit()
+                sys.exit()
+
+    P1.update()
+    E1.move()
+
+    DISPLAYSURF.fill(WHITE)
+    P1.draw(DISPLAYSURF)
+    E1.draw(DISPLAYSURF)
+
+    pygame.display.update()
+    FramePerSec.tick(FPS)
--- PythonPygame-1.py
+++ PythonPygame-1.py
@@ -1,45 +1,99 @@
+# 1 - https://www.qwant.com/?client=brz-moz&t=videos&q=pygame&o=0%3AWxEmflz009U
+# 2 - https://www.youtube.com/watch?v=W4cbtcyONag
+# 3 - https://www.youtube.com/watch?v=N56R1V5XZBw
+import sys
+import pygame
+
+# pygame.init()
+#
+# # Creating colors
+# BLUE = (0, 0, 255)
+# RED = (255, 0, 0)
+# GREEN = (0, 255, 0)
+# BLACK = (0, 0, 0)
+# WHITE = (255, 255, 255)
+#
+# screen = pygame.display.set_mode((400, 400))
+# screen.fill(WHITE)
+#
+# image = pygame.image.load("ball.png")
+# image = pygame.transform.rotozoom(image, 0, 0.2)
+#
+# x = 0
+# y = 0
+#
+# clock = pygame.time.Clock()
+#
+# while True:
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT:
+#             pygame.quit()
+#             sys.exit()
+#
+#     pressed = pygame.key.get_pressed()
+#     if pressed[pygame.K_LEFT]:
+#         x -= 1
+#     if pressed[pygame.K_RIGHT]:
+#         x += 1
+#     if pressed[pygame.K_UP]:
+#         y -= 1
+#     if pressed[pygame.K_DOWN]:
+#         y += 1
+#
+#     """
+#     On repeint en blanc le fond afin d'effacer les traces laisser par le deplacement du ballon.
+#     """
+#     screen.fill(WHITE)
+#     screen.blit(image, (x, y))
+#     pygame.display.flip()
+#     clock.tick(60)
+
 """
-Primitive Drawing Functions
+pygame.display.update()
+Les deux fonctions font la meme action elles mettent à jour les informations de l'écran en appliquant les
+changements faits. Cependant pygame.display.update() a une particularité, à l'aide d'un tuple de 4 valeurs 
+(x, y. width, height) ou d'un objet rect: pygame.Rect(x, y, width, height) nous pouvons définir la zone que nous
+souhaitons mettre à jour. Ainsi si nous utilisons la méthode sous cette forme pygame.display.update((0, 0, 150, 150)) 
+nous mettons à jour qu'un carré de 150 pixels par 150 pixels qui se trouve aux coordonnées 0, 0.
+pygame.display.flip() quand à lui ne prend aucun paramètre et permet de mettre à jour l'écran entier. Cependant
+flip a été codé avec certaine optimisations qui permettent de mettre à jour l'écran plus rapidement.
 """
-# https://inventwithpython.com/pygame/
-import pygame, sys
-from pygame.locals import *
+
+"""
+pygame en oriente objet
+"""
+
+
+class Game:
+    def __init__(self, screen):
+        self.screen = screen
+        self.clock = pygame.time.Clock()
+
+    #  Gestion des évènements
+    def handling_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+    # Gestion de logique et de mise à jour de chaque élément du jeu
+    def update(self):
+        pass
+
+    # Gestion de l'affichage
+    def display(self):
+        pass
+
+    # Boucle du jeu
+    def run(self):
+        while True:
+            self.handling_events()
+            self.update()
+            self.display()
+            self.clock.tick(60)
+
 
 pygame.init()
-
-# set up the window
-DISPLAYSURF = pygame.display.set_mode((500, 400))
-pygame.display.set_caption('Drawing')
-
-# set up colors
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
-
-# draw on the surface object
-DISPLAYSURF.fill(WHITE)
-pygame.draw.polygon(DISPLAYSURF, GREEN, ((146, 0), (291, 106), (236, 277), (56, 277), (0, 106)))
-pygame.draw.line(DISPLAYSURF, BLUE, (60, 60), (120, 60), 4)
-pygame.draw.line(DISPLAYSURF, BLUE, (120, 60), (60, 120))
-pygame.draw.line(DISPLAYSURF, BLUE, (60, 120), (120, 120), 4)
-pygame.draw.circle(DISPLAYSURF, BLACK, (300, 50), 20, 0)
-pygame.draw.ellipse(DISPLAYSURF, RED, (300, 250, 40, 80), 1)
-pygame.draw.rect(DISPLAYSURF, RED, (200, 150, 100, 50))
-
-pixObj = pygame.PixelArray(DISPLAYSURF)
-pixObj[480][380] = BLACK
-pixObj[482][382] = BLACK
-pixObj[484][384] = BLACK
-pixObj[486][386] = BLACK
-pixObj[488][388] = BLACK
-del pixObj
-
-# run the game loop
-while True:
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
-    pygame.display.update()
+screen = pygame.display.set_mode((1000, 720))
+game = Game(screen)
+game.run()
