b40723dd84f360cb132865cb791037d20c17fe7e
julienPalleau <jpalleau@gmail.com>
2022-08-05 15:48:25 +0200

    howto sprite

--- Game-Developpment/spriteExample.py
+++ Game-Developpment/spriteExample.py
@@ -1,7 +1,8 @@
 # https://www.youtube.com/watch?v=VO8rTszcW4s&list=PLsk-HSGFjnaH5yghzu7PcOzm9NhsW0Urw&index=1
-# Pygame template - skeleton for a new pygame project
+
 import pygame
 import random
+import os
 
 WIDTH = 800
 HEIGHT = 600
@@ -14,18 +15,30 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
+# set up assets folders
+game_folder = os.path.dirname(__file__)
+img_folder = os.path.join(game_folder, "img")
+
 
 class Player(pygame.sprite.Sprite):
     # sprite for the player
     def __init__(self):
         super().__init__()
-        self.image = pygame.Surface((50, 50))
-        self.image.fill(GREEN)
+        self.image = pygame.image.load(
+            os.path.join(img_folder, "p1_jump.png")).convert()  # it is necessary to use convert
+        # each time you load an image, as the image is optimized, otherwise it will go slower.
+        self.image.set_colorkey(BLACK)  # it is to make the black square around the character transparent
         self.rect = self.image.get_rect()
         self.rect.center = (WIDTH / 2, HEIGHT / 2)
+        self.y_speed = 5
 
     def update(self):
         self.rect.x += 5
+        self.rect.y += self.y_speed
+        if self.rect.bottom > HEIGHT - 200:
+            self.y_speed = -5
+        if self.rect.top < 200:
+            self.y_speed = 5
         if self.rect.left > WIDTH:
             self.rect.right = 0
 
